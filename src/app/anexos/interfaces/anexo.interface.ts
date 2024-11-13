c68e3b4715a3dab9e29070b4cad87b5ac1d66532
mozac <cesarmozareyes@gmail.com>
2024-11-13 12:27:25 -0500

    Add anexo and anexoDetail.interface.ts

---
+++ src/app/anexos/interfaces/anexo.interface.ts
@@ -0,0 +1,9 @@
+export interface Anexo {
+    key__key:     string;
+    key__anexo:   string;
+    registro:     Date;
+    status:       boolean;
+    last:         boolean;
+    duration_hrs: string;
+  }
+  
---
+++ src/app/anexos/interfaces/anexoDetail.interface.ts
@@ -0,0 +1,8 @@
+export interface AnexoDetail {
+    id: number;
+    key_id: string;
+    status: boolean;
+    registro: string;
+    first: boolean;
+    last: boolean;
+  }
