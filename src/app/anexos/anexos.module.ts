110dad31602187eedc8f8f64f62b8e3f60cacc35
mozac <cesarmozareyes@gmail.com>
2024-11-13 11:29:59 -0500

    setup initial

---
+++ src/app/anexos/anexos.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AnexosRoutingModule } from './anexos-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AnexosRoutingModule
+  ]
+})
+export class AnexosModule { }
---
+++ src/app/anexos/pages/anexo-detail/anexo-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anexo-detail',
+  templateUrl: './anexo-detail.component.html',
+  styleUrls: ['./anexo-detail.component.css']
+})
+export class AnexoDetailComponent {
+
+}
