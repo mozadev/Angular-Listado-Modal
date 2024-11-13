ec4836c4b5d712d3a939ff3965fdaa6aae7beb95
mozac <cesarmozareyes@gmail.com>
2024-11-13 11:37:24 -0500

    Add routes on app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
 
-
+  {
+    path: 'anexos',
+    loadChildren: () => import('./anexos/anexos.module').then(m =>m.AnexosModule),
+  },
+  {
+    path:'404',
+    component: Error404PageComponent
+  },
+  {
+    path: '',
+    redirectTo: 'anexos',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './anexos/shared/shared.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
