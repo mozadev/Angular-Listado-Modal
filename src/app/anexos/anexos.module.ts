10227485e34c1b3879ba921fae4b570e48eb6332
mozac <cesarmozareyes@gmail.com>
2024-11-13 12:25:07 -0500

    add AnexoDetailComponent on declarations-anexo.module

--- src/app/anexos/anexos.module.ts
+++ src/app/anexos/anexos.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { AnexosRoutingModule } from './anexos-routing.module';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { MaterialModule } from './material/material.module';
+import { AnexoDetailComponent } from './pages/anexo-detail/anexo-detail.component';
 
 
 
 @NgModule({
   declarations: [
-    ListPageComponent
+    ListPageComponent,
+    AnexoDetailComponent
 
   ],
   imports: [
