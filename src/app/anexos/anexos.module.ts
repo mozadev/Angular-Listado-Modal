b8d9d48737aabdde32e48c7e5c57400135d44a6b
mozac <cesarmozareyes@gmail.com>
2024-11-13 12:23:14 -0500

    add MaterialModule and AnexosRoutingModule on anexos.module.ts

--- src/app/anexos/anexos.module.ts
+++ src/app/anexos/anexos.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnexosRoutingModule } from './anexos-routing.module';
 import { ListPageComponent } from './pages/list-page/list-page.component';
+import { MaterialModule } from './material/material.module';
 
 
 
@@ -12,7 +13,8 @@ import { ListPageComponent } from './pages/list-page/list-page.component';
   ],
   imports: [
     CommonModule,
-    AnexosRoutingModule
+    AnexosRoutingModule,
+    MaterialModule,
   ]
 })
 export class AnexosModule { }
