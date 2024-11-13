235be3110ecf64a0b17246886029fb64f32bbb54
mozac <cesarmozareyes@gmail.com>
2024-11-13 12:19:15 -0500

    add routes on anexos-routing.modules

--- src/app/anexos/anexos-routing.module.ts
+++ src/app/anexos/anexos-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ListPageComponent } from './pages/list-page/list-page.component';
 
-
+const routes: Routes = [
+  {path: 'list', component: ListPageComponent},
+  {path: '**', redirectTo: 'list'},
+  {path: '', redirectTo: 'list', pathMatch: 'full' },
+]
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
-
 export class AnexosRoutingModule { }
