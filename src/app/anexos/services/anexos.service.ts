8ca77938c077e12dc41c46246dc9e4b6cb5eb4b1
mozac <cesarmozareyes@gmail.com>
2024-11-13 17:06:35 -0500

    add anexo service

--- src/app/anexos/services/anexos.service.ts
+++ src/app/anexos/services/anexos.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Anexo } from '../interfaces/anexo.interface';
+import { environments } from 'src/environments/environments';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class ServicesService {
 
-  constructor() { }
+interface AnexosResponse {
+  count: number;
+  up: number;
+  down: number;
+  up_rate: string;
+  down_rate: string;
+  data: Anexo[];
+}
+
+@Injectable({providedIn: 'root'})
+export class AnexosService {
+
+  private baseUrl: string = environments.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+
+
+
 }
