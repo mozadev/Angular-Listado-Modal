830aeea6e0c49c529d03943f2250fabd6579db0b
mozac <cesarmozareyes@gmail.com>
2024-11-13 12:21:50 -0500

    add material modules

--- src/app/anexos/anexos.module.ts
+++ src/app/anexos/anexos.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnexosRoutingModule } from './anexos-routing.module';
+import { ListPageComponent } from './pages/list-page/list-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListPageComponent
+
+  ],
   imports: [
     CommonModule,
     AnexosRoutingModule
