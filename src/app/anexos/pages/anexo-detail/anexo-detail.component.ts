ab46a22ba8f8bd886de8fac9d56b7e819f75688b
mozac <cesarmozareyes@gmail.com>
2024-11-18 17:31:42 -0500

    add detail component of each anexo

--- src/app/anexos/pages/anexo-detail/anexo-detail.component.ts
+++ src/app/anexos/pages/anexo-detail/anexo-detail.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { AnexosService } from '../../services/anexos.service';
+import { AnexoDetail } from '../../interfaces/anexoDetail.interface';
+
+interface AnexoDetailData{
+  duration_hrs: string;
+  records: AnexoDetail[]; 
+}
 
 @Component({
   selector: 'app-anexo-detail',
   templateUrl: './anexo-detail.component.html',
   styleUrls: ['./anexo-detail.component.css']
 })
-export class AnexoDetailComponent {
 
+export class AnexoDetailComponent implements  OnInit{
+  
+  constructor(
+    public dialogRef: MatDialogRef<AnexoDetailComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: AnexoDetailData
+  ){}
+    
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  closeDialog():void {
+    this.dialogRef.close()
+  }
 }
